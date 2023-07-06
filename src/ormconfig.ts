da18365bb068e948ef7f04e71ab0dde9198ac300
ArtemTodor <t.emiktody@gmail.com>
2023-07-06 11:39:25 +0300

    init

---
+++ src/ormconfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'firstpro',
+  password: '3301',
+  database: 'firstpro',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export default config;
